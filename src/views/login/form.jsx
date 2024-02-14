0f6a4bc59f19a86b9ebc7c99aedc7b8a4dd00975
MuhammadUsman393 <m.usman.usman393@gmail.com>
2024-02-14 15:41:00 +0500

    change base_url of api

--- src/views/login/form.jsx
+++ src/views/login/form.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { BASE_URL } from "../utils/axios-instance";
+import { BASE_URL } from "../../utils/axios-instance";
 
 const Form = () => {
   const [email, setEmail] = useState("");
